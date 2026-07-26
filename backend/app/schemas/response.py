b306aefb63f236ab4c2903dca2fb4157ad50142e
KiriLLchik07 <kirill3456777@mail.ru>
2026-07-26 15:51:33 +0600

    fix: LoginResponse is not needed yet

--- backend/app/schemas/response.py
+++ backend/app/schemas/response.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
-from uuid import UUID
+# from pydantic import BaseModel
+# from uuid import UUID
 
-class LoginResponse(BaseModel):
-    user_id: UUID
-    success: str
+# class LoginResponse(BaseModel):
+#     user_id: UUID
+#     success: str
 
     # Нужны еще поля
