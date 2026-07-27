1094aa77893507774195de13c55a18b0b8eca941
KiriLLchik07 <kirill3456777@mail.ru>
2026-07-27 12:52:09 +0600

    feat: Create user session service and cookie logic

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -4,19 +4,20 @@ from fastapi import HTTPException
 from backend.app.schemas.request import UserRegisterRequest, UserLoginRequest
 from backend.app.repositories.user_repository import UserRepository
 from backend.app.serializer.serializer import user_to_dto
+from backend.app.models.user import User
 
 class AuthService:
     def __init__(self, db_session: Session) -> None:
         self.db_session = db_session
         self.repository = UserRepository(db_session)
 
-    def login_user(self, payload: UserLoginRequest) -> dict:
+    def login_user(self, payload: UserLoginRequest) -> User:
         user = self.repository.get_user_by_email(payload.user_mail)
         
         if user is None or user.user_password != payload.user_password:
             raise HTTPException(401, "Пользователь с такими данными не найден. Проверьте почту или пароль. Если вы еще не регистрировались, то сделайте это!")
 
-        return user_to_dto(user)
+        return user
 
     def register_user(self, payload: UserRegisterRequest) -> dict:
         if_user = self.repository.get_user_by_email(payload.user_mail)
