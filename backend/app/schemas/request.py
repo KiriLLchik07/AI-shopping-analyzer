9a4fb8495ebec10d0746988e46288d738134dbc9
KiriLLchik07 <kirill3456777@mail.ru>
2026-08-04 10:31:59 +0600

    feat: add validation and normalization for password and mail

--- backend/app/schemas/request.py
+++ backend/app/schemas/request.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
+from backend.app.schemas.validation import NormalizedEmail, RegistrationPassword, LoginPassword
 
 class UserLoginRequest(BaseModel):
-    user_mail: str
-    user_password: str
+    user_mail: NormalizedEmail
+    user_password: LoginPassword
 
 class UserRegisterRequest(BaseModel):
     user_name: str
     user_surname: str
-    user_mail: str
-    user_password: str
+    user_mail: NormalizedEmail
+    user_password: RegistrationPassword
 
---
+++ backend/app/schemas/validation.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+from pydantic import BeforeValidator, EmailStr, Field, AfterValidator
+
+def normalize_email(value: object) -> object:
+    if isinstance(value, str):
+        return value.strip().casefold()
+
+    return value
+
+def validate_registration_password(password: str):
+    if password.isspace():
+        raise ValueError("Пароль не может быть пустым")
+
+NormalizedEmail = Annotated[
+    EmailStr,
+    BeforeValidator(normalize_email),
+]
+
+RegistrationPassword = Annotated[
+    str,
+    Field(min_length=12, max_length=128),
+    AfterValidator(validate_registration_password)
+]
+
+LoginPassword = Annotated[
+    str,
+    Field(min_length=1, max_length=128),
+]
